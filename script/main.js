f1d811888d307ab1c9cf52db44528d3f81094500
Akosile Tayo <akosilety9@gmail.com>
2020-12-31 13:13:24 -0800

    Did the Mobile Verison of the app

--- script/main.js
+++ script/main.js
@@ -1,18 +1,32 @@
 //This shows the rules button when clicked
 const showRule = document.querySelector("footer button");
 const rule = document.querySelector(".show__rule ");
+//This shows the rules button when clicked
 
 //This happens when paper,scissors,rock button is clicked 
-const mainGame = document.querySelector(".main_games");
+const traingle = document.querySelector(".main_games--triangle");
 
+//THe div that contains the images(Rock Paper And Scissors)
+const gameContainer = document.querySelector(".js-flex");
+const mainGame = document.querySelectorAll(".js-flex");
+
+// When Rock Paper and scissors is Pressed
+mainGame.forEach(game => {
+    game.addEventListener("click", rockOrPaperOrScissorsClicked)
+});
+
+// When Either Of Them Is Clicked
+function rockOrPaperOrScissorsClicked() {
+    gameContainer.style.display = "none"
+    console.log(this);
+}
 showRule.addEventListener("click", openRules)
 
-// OPens and closes rule.
+// OPens and closes rule button.
 function openRules() {
     rule.classList.toggle("active");
 
     if (rule.classList.contains("active")) {
-
         //When the Rules Button is clicked,thsi happens
         this.style.border = "transparent";
         this.style.fontSize = "20px";
