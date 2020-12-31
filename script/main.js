6da8599813764eaa92e6f0ef3dab87aaf1d34a5d
Akosile Tayo <akosilety9@gmail.com>
2020-12-31 06:20:49 -0800

    Added the rules

---
+++ script/main.js
@@ -0,0 +1,31 @@
+//THis shows the rules when clicked
+const showRule = document.querySelector("footer button");
+const rule = document.querySelector(".show__rule ");
+
+
+showRule.addEventListener("click", openRules)
+
+// OPens and closes rule.
+function openRules() {
+    rule.classList.toggle("active");
+
+    if (rule.classList.contains("active")) {
+
+        //When the Rules Button is clicked,thsi happens
+        this.style.border = "transparent";
+        this.style.fontSize = "15px";
+        this.style.fontWeight = "300";
+        this.innerHTML = "X";
+        this.style.transition = "0.4s ease border,0.4s ease color"
+        this.style.color = "hsl(229, 25%, 31%)";
+        return;
+        
+    }
+    //When the Rules Button is clicked again ,thsi happens
+
+    this.style.border = ` 2px solid hsl(229, 25%, 31%)`;
+    this.style.transition = "0.4s ease border,0.4s ease color"
+    this.style.fontSize = "10px";
+    this.innerHTML = "rules";
+    this.style.color = "#fff";
+}
