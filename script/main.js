dfb838865c8fc4aadc01aac7fcbf0342c66c703b
Akosile Tayo <akosilety9@gmail.com>
2020-12-31 11:10:38 -0800

    Did the Mobile Verison of the app

--- script/main.js
+++ script/main.js
@@ -1,7 +1,9 @@
-//THis shows the rules when clicked
+//This shows the rules button when clicked
 const showRule = document.querySelector("footer button");
 const rule = document.querySelector(".show__rule ");
 
+//This happens when paper,scissors,rock button is clicked 
+const mainGame = document.querySelector(".main_games");
 
 showRule.addEventListener("click", openRules)
 
@@ -13,21 +15,21 @@ function openRules() {
 
         //When the Rules Button is clicked,thsi happens
         this.style.border = "transparent";
-        this.style.fontSize = "15px";
+        this.style.fontSize = "20px";
         this.style.fontWeight = "300";
         this.innerHTML = "X";
         this.style.transition = "0.4s ease border,0.4s ease color"
         this.style.color = "hsl(229, 25%, 31%)";
         return;
-        
+
     }
-    //When the Rules Button is clicked again ,thsi happens
+    //When the Rules Button is clicked again (X) ,this happens
 
     this.style.border = ` 3px solid hsl(229, 25%, 31%)`;
     this.style.transition = "0.4s ease border,0.4s ease color"
     this.style.fontSize = "12px";
     this.innerHTML = "rules";
     this.style.fontWeight = "700";
-    this.style.color = "#fff";
-    this.style.letterSpacing = "2px";
+    this.style.color = "#3b4363";
+    this.style.letterSpacing = "1px";
 }
